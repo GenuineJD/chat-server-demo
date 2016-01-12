f9fd057616a82b10e68463bfd82f239614e6e713
Jack Minster <jack.minster@designkitchen.com>
2016-01-12 11:48:36 -0600

    tweaks to gulp testing task

--- gulp/tasks/test.js
+++ gulp/tasks/test.js
@@ -3,8 +3,15 @@ var mocha = require('gulp-mocha');
 var gulpSequence = require('gulp-sequence');
  
 gulp.task('js-test', function () {
+	console.log('running js tests...');
 	return gulp.src('src/tests/**/*.js', {read: false})
-			.pipe(mocha({reporter: 'nyan'}));
+			.pipe(mocha({reporter: 'nyan'}))
+			.once('error', function() {
+				process.exit(1);
+			})
+			.once('end', function() {
+				process.exit();
+			});
 });
 
 gulp.task('test', gulpSequence('server','js-test'));
